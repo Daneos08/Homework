842f5bdf2224220bd93000eb1a36c06f5055c17a
Daneos08 <113821056+Daneos08@users.noreply.github.com>
2023-03-02 09:54:19 +0000

    changed cat class to dog class (Homework 2)

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello teacher")
+class Person:
+  height = 170
+  name = "Name"
+  is_sad = True
+  
+  def __init__(self, name , height):
+    self.name = name
+    self.height = height
+
+
+class Dog:
+  color = "Black"
+
+  def __init__(self, name):
+    self.name = name
+  def play_w_hooman(self, human):
+    human.is_sad = False
+
+
+
+me = Person('Deenis', 179)
+me_pet = Dog("Sharik")
+
+you = Person('Macsim', 176)
+
+
+print(me.is_sad)
+me_pet.play_w_hooman(me)
+print("Сумний - ",me.is_sad)
+
+
+print(you.is_sad)
+me_pet.play_w_hooman(you)
+print("Сумний - ",you.is_sad)
